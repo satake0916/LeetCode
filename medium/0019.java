8885d2086c9a8427483800a89c63933299acada9
satake0916 <38389682+satake0916@users.noreply.github.com>
2020-01-11 17:49:42 +0900

    Update 0019.java

--- medium/0019.java
+++ medium/0019.java
@@ -23,3 +23,20 @@ class Solution {
         return head;
     }
 }
+
+//one path solution
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for(int i = 0; i < n+1; i++) second = second.next;
+        while(second != null){
+            first = first.next;
+            second = second.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+}

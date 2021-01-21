ed081cdd8f4c418098d2a74370002eab75e80614
satake0916 <38389682+satake0916@users.noreply.github.com>
2021-01-22 02:29:30 +0900

    Create 23_Binary Tree Zigzag Level Order Traversal.java

---
+++ Arai60/23_Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        rec(ans, root, 0);
+        for(int i = 0; i < ans.size(); i++){
+            if(i%2 == 1){
+                Collections.reverse(ans.get(i));
+            }
+        }
+        return ans;        
+    }
+    
+    private void rec(List<List<Integer>> ans, TreeNode cur, int level){
+        if(ans.size() <= level){
+            ans.add(new ArrayList<Integer>());
+        }
+        ans.get(level).add(cur.val);
+        if(cur.left != null) rec(ans, cur.left, level+1);
+        if(cur.right != null) rec(ans, cur.right, level+1);
+    }
+}

0686268089f19f6f4b403b48c39a316db463591b
satake0916 <38389682+satake0916@users.noreply.github.com>
2021-01-23 20:02:13 +0900

    Create 56_Kth Largest Element in a Stream.java

---
+++ Arai60/56_Kth Largest Element in a Stream.java
@@ -0,0 +1,22 @@
+class KthLargest {
+    Queue<Integer> q;
+    int k;
+
+    public KthLargest(int k, int[] nums) {
+        q = new PriorityQueue<>();
+        this.k = k;
+        for(int i: nums) add(i);
+    }
+    
+    public int add(int val) {
+        q.offer(val);
+        if(q.size() > k) q.poll();
+        return q.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
